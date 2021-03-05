88740e0c0da4484d52210aee8108142dd6fa108f
Markus Staab <m.staab@complex-it.de>
2021-03-05 10:25:48 +0100

    added autoloading

--- src/feature-test.php
+++ src/feature-test.php
@@ -4,6 +4,11 @@ use Behat\Behat\Context\Context;
 
 class TrackingBusinessContext implements Context
 {
+    /**
+     * @var \integration\i1
+     */
+    private $i;
+
     public function myTest() {
         new integration\i1();
     }
